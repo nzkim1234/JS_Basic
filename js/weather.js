0c6cbbb2b81cfdbae16063f1ccb3c863a42f2733
nzkim1234 <nzkim1234@naver.com>
2022-01-04 02:14:59 +0900

    JS_Basic

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = 'fcb11506955b563dd3220edfb90e0277'
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+    console.log(url)
+}
+
+function onGoError() {
+    alert("Can't find you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGoError);
